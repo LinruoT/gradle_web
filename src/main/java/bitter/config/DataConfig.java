c05418d96d6e3ab77cef82c41a2d91e47f33bba8
billy <linruot@outlook.com>
2019-03-28 03:09:48 +0800

    chapter 10 add: use database connection pool

--- src/main/java/bitter/config/DataConfig.java
+++ src/main/java/bitter/config/DataConfig.java
@@ -1,5 +1,6 @@
 package bitter.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcOperations;
@@ -18,7 +19,7 @@ public class DataConfig {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
                 .addScript("schema.sql").build();
     }
-    @Bean
+//    @Bean //使用jdbc
     public DataSource dataSourceBillyvpsMysql() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -29,7 +30,18 @@ public class DataConfig {
         ds.setPassword("billy11111111");
         return ds;
     }
-
+    @Bean //使用连接池
+    public BasicDataSource dataSourceDBCP() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://billyvps.cf:3306/bitter" +
+                "?useUnicode=true&characterEncoding=UTF-8" +
+                "&serverTimezone=Asia/Shanghai&useSSL=false");
+        ds.setUsername("billyvps");
+        ds.setPassword("billy11111111");
+        ds.setInitialSize(5);
+        return ds;
+    }
     @Bean
     public JdbcOperations jdbcTemplate(DataSource dataSource) {
         return  new JdbcTemplate(dataSource);
